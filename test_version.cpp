0eed7dfcf9eeb6f7830e3e8d42a1096fa7a007d9
Artem <artem-kopylov@ya.ru>
2021-10-03 01:08:26 +0300

    add environment variable CTEST_OUTPUT_ON_FAILURE = 1 to yml and check version forwarding to test

--- test_version.cpp
+++ test_version.cpp
@@ -9,6 +9,10 @@ BOOST_AUTO_TEST_SUITE(test_version)
 BOOST_AUTO_TEST_CASE(test_valid_version)
 {
     BOOST_CHECK(version() > 0);
+    if (boost::unit_test::framework::master_test_suite().argc > 1)
+    {
+        BOOST_CHECK(version() == atoi(boost::unit_test::framework::master_test_suite().argv[1]));
+    }
 }
 
 }
